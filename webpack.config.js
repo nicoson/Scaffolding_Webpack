a7a3b5a78b0cfb3afb6782decd7f1a9704e1dfe6
nicoson <nicoson@163.com>
2017-06-11 15:25:43 +0800

    version 0.0.1

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+	/***********************\
+	|	config source map
+	\***********************/
+	devtool: 'eval-source-map',//配置生成Source Maps，选择合适的选项
+
+
+	/************************************\
+	|	config entrance and output dir
+	\************************************/
+	entry: __dirname + "/app/main.js",//已多次提及的唯一入口文件
+	output: {
+		path: __dirname + "/public",//打包后的文件存放的地方
+		filename: "bundle.js"//打包后输出文件的文件名
+	},
+
+
+	/********************\
+	|	config loaders
+	\********************/
+	module: {//在配置文件里添加JSON loader
+		rules: [
+			{
+				test: /\.json$/,
+				loader: "json-loader"
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',//在webpack的module部分的loaders里进行配置即可
+				query: {
+					presets: ['es2015','react']
+				}
+			}
+		]
+	},
+
+
+	/********************\
+	|	config server
+	\********************/
+	devServer: {
+		contentBase: "./public",//本地服务器所加载的页面所在的目录
+		//color: true,//终端中输出结果为彩色
+		historyApiFallback: true,//不跳转
+		inline: true//实时刷新
+	} 
+}
